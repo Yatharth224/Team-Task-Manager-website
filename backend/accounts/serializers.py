b858dc4552ad4a03f9b5a052df34208103d14499
Your Name <yatharthchouhan22@gmail.com>
2026-05-09 23:23:49 +0530

    seroalizer.py

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'password')
+
+    def create(self, validated_data):
+        # use create_user so password gets hashed properly
+        user = User.objects.create_user(
+            username=validated_data['email'],
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password'],
+        )
+        return user
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'created_at')
+        read_only_fields = ('id', 'email', 'created_at')
+
+
+class BasicUserSerializer(serializers.ModelSerializer):
+    """Used when embedding user info inside other serializers"""
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email')
